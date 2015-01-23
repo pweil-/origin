ab2416eac405775ddd0588bff39985e3624c2584
Paul Weil <pweil@redhat.com>
2015-01-23 10:11:15 -0500

    router TLS

---
+++ plugins/router/template/certmanager.go
@@ -0,0 +1,66 @@
+package templaterouter
+
+import (
+	"github.com/golang/glog"
+	"io/ioutil"
+
+	routeapi "github.com/openshift/origin/pkg/route/api"
+)
+
+type certManager struct{}
+
+// write certificates for edge and reencrypt termination by appending the key, cert, and ca cert
+// into a single <host>.pem file.  Also write <host>_pod.pem file if it is reencrypt termination
+func (cm *certManager) writeCertificatesForConfig(config *ServiceAliasConfig) error {
+	if len(config.Certificates) > 0 {
+		if config.TLSTermination == routeapi.TLSTerminationEdge || config.TLSTermination == routeapi.TLSTerminationReencrypt {
+			certObj, ok := config.Certificates[config.Host]
+
+			if ok {
+				newLine := []byte("\n")
+				cert := []byte(certObj.Contents)
+				key := []byte(certObj.PrivateKey)
+				dat := append(key, newLine...)
+				dat = append(dat, cert...)
+
+				caCertObj, caOk := config.Certificates[config.Host+CaCertPostfix]
+
+				if caOk {
+					dat = append(dat, newLine...)
+					dat = append(dat, caCertObj.Contents...)
+				}
+
+				cm.writeCertificate(CertDir, config.Host, dat)
+			}
+		}
+
+		if config.TLSTermination == routeapi.TLSTerminationReencrypt {
+			destCertKey := config.Host + DestCertPostfix
+			destCert, ok := config.Certificates[destCertKey]
+
+			if ok {
+				cm.writeCertificate(CaCertDir, destCertKey, []byte(destCert.Contents))
+			}
+		}
+	}
+
+	return nil
+}
+
+// writeCertificate writes to disk
+func (cm *certManager) writeCertificate(directory string, id string, cert []byte) error {
+	fileName := directory + id + ".pem"
+	err := ioutil.WriteFile(fileName, cert, 0644)
+
+	if err != nil {
+		glog.Errorf("Error writing certificate file %v: %v", fileName, err)
+		return err
+	}
+
+	return nil
+}
+
+func (cm *certManager) deleteCertificatesForConfig(config *ServiceAliasConfig) error {
+	//TODO
+	return nil
+}
